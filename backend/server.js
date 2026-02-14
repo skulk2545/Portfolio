450d8b688b7842031c0b59d11721b1ad7dae8709
skulk2545 <kulkarnishreya179@gmail.com>
2026-02-14 11:59:50 +0530

    Modify email sending logic in server.js
    Updated email sending functionality to include phone number in the message and changed email format.

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
 app.post("/send", async (req, res) => {
-  const { name, email, message } = req.body;
+  const { name, email, phone, message } = req.body;
 
   try {
     const transporter = nodemailer.createTransport({
@@ -16,22 +18,27 @@ app.post("/send", async (req, res) => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+      },
+      tls: {
+        rejectUnauthorized: false
       }
     });
 
+
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
+      from: `"Portfolio Contact" <kshreya0725@gmail.com>`,
+      to: "kshreya0725@gmail.com",
       subject: "New Project Inquiry",
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+      text: `Name: ${name}\nEmail: ${email}\n Phone: ${phone}\n Message: ${message}`
     });
 
-    res.status(200).json({ success: true });
+    console.log("EMAIL SENT");
+    res.json({ success: true });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false });
+    console.error("EMAIL ERROR:", error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running"));
+app.listen(5000, () => console.log("Server running on http://192.168.1.36:5000"));
